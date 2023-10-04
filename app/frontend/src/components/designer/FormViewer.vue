a3f8b50f094e3599aa33795679b8d0a0cc338f04
Branko Bajic <eisoftware@mailfence.com>
2023-10-03 22:35:55 -0700

    set approved (again)

--- app/frontend/src/components/designer/FormViewer.vue
+++ app/frontend/src/components/designer/FormViewer.vue
@@ -1135,8 +1135,8 @@ export default {
     onCustomEvent(event) {
       if (event.type === "calculatorApprove"){
         this.submission.data.customEvent = true;
+        this.submission.data.approved = true;
         if (this.submission.data.container){
-        this.submission.data.container.approved = true;
         this.submission.data.container.approvedBy = `${this.submission.data.userInfo.username}@${this.submission.data.userInfo.idp}`;
         this.submission.data.container.approvedDate = new moment();
         }
