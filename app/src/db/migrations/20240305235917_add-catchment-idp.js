67189157a88d7da38c56f5c549b4738b36a8ea46
Branko Bajic <branko@eisi.dev>
2024-03-05 16:02:58 -0800

    add migration

---
+++ app/src/db/migrations/20240305235917_add-catchment-idp.js
@@ -0,0 +1,23 @@
+const CREATED_BY = 'elmsd-02';
+
+exports.up = function(knex) {
+  return Promise.resolve()
+    .then(() => {
+      return knex('identity_provider').insert([
+        {
+          createdBy: CREATED_BY,
+          code: 'bceid-catchment',
+          display: 'Basic or Business BCeID (Catchment Protected)',
+          active: true,
+          idp: 'bceid'
+        }
+      ]);
+    });
+};
+  
+exports.down = function(knex) {
+  return Promise.resolve()
+    .then(() => {
+      return knex('identity_provider').where('createdBy', CREATED_BY).del();
+    });
+};
